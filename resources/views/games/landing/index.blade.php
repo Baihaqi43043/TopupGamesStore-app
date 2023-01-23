9f674ab05d52b98344f6ceb4848eb9998c0170a4
baihaqi43043 <baihaqi43043@gmail.com>
2023-01-23 17:55:25 +0700

    intitial commit

---
+++ resources/views/games/landing/index.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.navapp')
+@push('custom-style')
+<style>
+
+</style>
+@endpush
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <h1>halo ini</h1>
+    </div>
+</div>
+@endsection
