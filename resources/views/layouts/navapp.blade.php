10d18acd5b91e83ab34296505e2e3e44c69e7908
RahmatSiddiqi <rahmatsiddiq946@gmail.com>
2023-01-30 15:04:44 +0700

    navbar commit

--- resources/views/layouts/navapp.blade.php
+++ resources/views/layouts/navapp.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -13,12 +14,42 @@
 
     @stack('custom-style')
 </head>
+
 <body>
+    <nav class="navbar navbar-expand-lg sticky-top navbar-light bg-light shadow">
+        <div class="container-fluid">
+            <div class="dflex">
+                <a class="navbar-brand" href="#">Navbar</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                    aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+            </div>
+
+            <div class="collapse navbar-collapse text-right" id="navbarNav">
+                <ul class="navbar-nav ms-auto me-5 ">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Features</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Pricing</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link disabled">Disabled</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
     <main class="py-4">
         @yield('content')
     </main>
-@stack('custom-script')
+    @stack('custom-script')
 
-@stack('link-script')
+    @stack('link-script')
 </body>
+
 </html>
